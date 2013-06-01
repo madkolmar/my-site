b61767e2fba863e18767fad8df5df9d0968408ce
XoJIog <stan@stan-HOME.(none)>
2013-06-01 16:44:04 +0400

    port was changed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = require("url");
+var port = process.env.port || 5000
 
 function start(route, handle) {
 	function onRequest(request, response) {
@@ -7,7 +8,7 @@ function start(route, handle) {
 		console.log("Request for " + pathname + " received.");
 		    route(handle, pathname, response);
 		}
-	    http.createServer(onRequest).listen(8888);
+	    http.createServer(onRequest).listen(port);
 		console.log("Server has started.");
 	}	
 
