762afa1eb08e1c30f7f2804dcea43ec397b23c77
XoJIog <stan@stan-HOME.(none)>
2013-06-17 22:04:26 +0400

    port upper case

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var url = require("url");
-var port = process.env.port || 5000;
+var port = process.env.PORT || 5000;
 
 function start(route, handle) {
 	function onRequest(request, response) {
@@ -9,7 +9,7 @@ function start(route, handle) {
 		    route(handle, pathname, response);
 		}
 	    http.createServer(onRequest).listen(parseInt(port));
-		console.log("Heroku says: port is " + process.env.port);
+		console.log("Heroku says: port is " + process.env.PORT);
 		console.log("Server has started at port " + port);
 	}	
 
